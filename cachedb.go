3e40458729df222c17093627cdf10d5e7b1ff5c5
zlyuancn <zlyuancn@163.com>
2020-04-14 19:08:11 +0800

    修改ICacheDB.Get对空数据的描述

--- cachedb.go
+++ cachedb.go
@@ -18,7 +18,7 @@ type ICacheDB interface {
     // 实现缓存数据库接口的结构应该主动考虑 v 值为 nil(空数据) 如何保存才能在获取时判断它是 NilData
     Set(query *Query, v interface{}, ex time.Duration) error
     // 获取一个值, 缓存数据库中不存在应该返回 ErrNoEntry
-    // 如果是空数据应该返回 nil,nil 或者返回 NilData 错误
+    // 如果是nil或空数据应该返回 NilData 错误
     Get(query *Query, a interface{}) (interface{}, error)
     // 删除一个key
     Del(query *Query) error
