39e04108954698e40b05fd3ec22696c0e9b4c625
zlyuancn <zlyuancn@163.com>
2020-04-15 16:50:54 +0800

    添加redis_hash

---
+++ cachedb/redis_hash/c.go
@@ -0,0 +1,132 @@
+/*
+-------------------------------------------------
+   Author :       Zhang Fan
+   date：         2020/4/15
+   Description :
+-------------------------------------------------
+*/
+
+package redis_hash
+
+import (
+    "crypto/md5"
+    "encoding/hex"
+    "time"
+
+    "github.com/afex/hystrix-go/hystrix"
+    rredis "github.com/go-redis/redis"
+    "github.com/zlyuancn/zerrors"
+
+    "github.com/zlyuancn/zbec"
+    "github.com/zlyuancn/zbec/codec"
+)
+
+var _ zbec.ICacheDB = (*RedisWrap)(nil)
+
+type RedisWrap struct {
+    cdb        rredis.UniversalClient
+    codec      codec.ICodec
+    md5_params bool
+    qfname     string // qf是断路器符号
+}
+
+func Wrap(db rredis.UniversalClient, opts ...Option) *RedisWrap {
+    m := &RedisWrap{
+        cdb:        db,
+        codec:      codec.GetCodec(codec.DefaultCodecType),
+        md5_params: true,
+    }
+    for _, o := range opts {
+        o(m)
+    }
+    return m
+}
+
+func (m *RedisWrap) Set(query *zbec.Query, v interface{}, ex time.Duration) (err error) {
+    if v == nil { // 不缓存空数据
+        return nil
+    }
+
+    bs, err := m.codec.Encode(v)
+    if err != nil {
+        return zerrors.WrapSimplef(err, "编码失败 %T", v)
+    }
+    return m.do(func() error {
+        return m.cdb.HSet(query.Space(), m.makeKey(query), bs).Err()
+    })
+}
+
+func (m *RedisWrap) Get(query *zbec.Query, a interface{}) (interface{}, error) {
+    var data []byte
+    var err error
+    empty := false
+    err = m.do(func() error {
+        bs, e := m.cdb.HGet(query.Space(), m.makeKey(query)).Bytes()
+        data = bs
+        if e == rredis.Nil {
+            empty = true
+            return nil
+        }
+        return e
+    })
+
+    if err != nil {
+        return nil, zerrors.WithSimple(err)
+    }
+    if empty {
+        return nil, zbec.ErrNoEntry
+    }
+    if len(data) == 0 {
+        return nil, zbec.NilData
+    }
+
+    err = m.codec.Decode(data, a)
+    if err != nil {
+        return nil, zerrors.WrapSimplef(err, "解码失败 %T", a)
+    }
+
+    return a, nil
+}
+
+func (m *RedisWrap) Del(query *zbec.Query) error {
+    return m.do(func() error {
+        err := m.cdb.HDel(query.Space(), m.makeKey(query)).Err()
+        if err == rredis.Nil {
+            return nil
+        }
+        return err
+    })
+}
+
+func (m *RedisWrap) DelSpaceData(space string) error {
+    return m.do(func() error {
+        err := m.cdb.Del(space).Err()
+        if err == rredis.Nil {
+            return nil
+        }
+        return err
+    })
+}
+
+func (m *RedisWrap) makeKey(query *zbec.Query) string {
+    if m.md5_params {
+        return string(makeMd5(query.Path()))
+    }
+    return query.Path()
+}
+
+func makeMd5(text string) []byte {
+    m := md5.New()
+    m.Write([]byte(text))
+    src := m.Sum(nil)
+    dst := make([]byte, hex.EncodedLen(len(src)))
+    hex.Encode(dst, src)
+    return dst
+}
+
+func (m *RedisWrap) do(fn func() error) error {
+    if m.qfname == "" {
+        return fn()
+    }
+    return hystrix.Do(m.qfname, fn, nil)
+}
---
+++ cachedb/redis_hash/opt.go
@@ -0,0 +1,36 @@
+/*
+-------------------------------------------------
+   Author :       Zhang Fan
+   date：         2020/4/15
+   Description :
+-------------------------------------------------
+*/
+
+package redis_hash
+
+import (
+    "github.com/zlyuancn/zbec/codec"
+)
+
+type Option func(m *RedisWrap)
+
+// 设置编码器
+func WithCodecType(ctype codec.CodecType) Option {
+    return func(m *RedisWrap) {
+        m.codec = codec.GetCodec(ctype)
+    }
+}
+
+// 设置断路器名, 空名称表示不使用断路器
+func WithHystrixName(qfname string) Option {
+    return func(m *RedisWrap) {
+        m.qfname = qfname
+    }
+}
+
+// 将query的params做md5, 默认为true
+func WithMd5QueryParams(b bool) Option {
+    return func(m *RedisWrap) {
+        m.md5_params = b
+    }
+}
